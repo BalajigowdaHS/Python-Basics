c1ec336acb48b37807c2a3c1d731c6c3a1edce0f
Balajigowda HS <86897591+BalajigowdaHS@users.noreply.github.com>
2022-10-27 20:15:16 +0530

    Create Variable Exercise.py

---
+++ Variable Exercise.py
@@ -0,0 +1,14 @@
+##Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+
+birth_year = 2000
+current_year = 2022
+current_age = current_year - birth_year
+print("current age is ", current_age)
+
+##Store your first, middle and last name in three different variables and then print your full name using these variables
+
+F_name = 'Balaji'
+M_name = 'gowda'
+L_name = ' H S'
+print("Full name is", F_name+M_name+L_name)
+
