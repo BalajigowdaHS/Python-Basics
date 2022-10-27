c655376d7f9a4c3cd3cdfa02a267ac7cef600a7a
Balajigowda HS <86897591+BalajigowdaHS@users.noreply.github.com>
2022-10-27 20:31:43 +0530

    Update If statement Exercise.py

--- If statement Exercise.py
+++ If statement Exercise.py
@@ -21,3 +21,23 @@ elif city in bangladesh:
     print(f"The {city} is in Bangladesh")
 else :
     print("The country is not Mentioned in the list")
+    
+    ## 1.2) Write a program that asks user to enter two cities and it tells you if they both are in same country or not.
+# For example if I enter mumbai and chennai, it will print "Both cities are in India"
+# but if I enter mumbai and dhaka it should print "They don't belong to same country"
+
+india = ["mumbai", "banglore", "chennai", "delhi"]
+pakistan = ["lahore","karachi","islamabad"]
+bangladesh = ["dhaka", "khulna", "rangpur"]
+
+city1 = input("Enter the city name")
+city2 = input("Enter the city name")
+
+if city1 in india and city2 in india:
+    print("Both cities are in india")
+elif city1 in pakistan and city2 in pakistan:
+    print("Both cities are in pakistan")
+elif city1 in bangladesh and city2 in bangladesh:
+    print("Both cities are in bangladesh")
+else:
+    print("They don't belong to same country")
